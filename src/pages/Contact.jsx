21e784da23c98bea93c8ed53c361d64d03a1e3d0
Itsarun01 <hey.arunkumar05@gmail.com>
2024-12-22 16:22:35 +0530

    contact page done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,46 @@
 const Contact = () => {
+  
+  const handleFormSubmit = (formData) => {
+    const formInputData = Object.fromEntries(formData.entries());
+    console.log(formInputData);
+  };
+
   return (
     <>
-      <div>
-        <h1>Contact</h1>
-      </div>
+      <section className="section-contact">
+        <h2 className="container-title">Contact Us</h2>
+        <div className="contact-wrapper container">
+          <form action={handleFormSubmit}>
+            <input
+              type="text"
+              name="username"
+              className="form-control"
+              required
+              placeholder="Enter Your Name"
+              autoComplete="false"
+            />
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              required
+              placeholder="Enter Your Email"
+              autoComplete="false"
+            />
+            <textarea
+              name="message"
+              rows="10"
+              className="form-control"
+              required
+              placeholder="Enter Your Message"
+              autoComplete="false"
+            ></textarea>
+            <button type="submit" value="send" className="submit-btn">
+              Send
+            </button>
+          </form>
+        </div>
+      </section>
     </>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "../Components/UI/HeroSection";
 import About from "./About";
+import Contact from "./Contact";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <About />
+      <Contact />
     </>
   );
 };
