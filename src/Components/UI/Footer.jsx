60493d8bfe30586c899065204754e7a8fcf05d91
Itsarun01 <hey.arunkumar05@gmail.com>
2024-12-22 17:27:45 +0530

    footer section done...

--- src/Components/UI/Footer.jsx
+++ src/Components/UI/Footer.jsx
@@ -1,7 +1,41 @@
+import FooterData from "../../api/FooterData.json";
+import {FaLocationDot} from "react-icons/fa6";
+import {BiSolidPhoneCall} from "react-icons/bi";
+import {MdMarkEmailUnread} from "react-icons/md";
+import {NavLink} from "react-router-dom";
 const Footer = () => {
+  const footerIcons = {
+    MdPlace: <FaLocationDot />,
+    IoCallSharp: <BiSolidPhoneCall />,
+    TbMailPlus: <MdMarkEmailUnread />,
+  };
+
   return (
     <>
-      <h1>Footer</h1>
+      <footer className="footer-section">
+        <div className="container grid grid-three-col">
+          {FooterData.map((Data, i) => {
+            const {icon, title, details} = Data;
+            return (
+              <div className="footer-contact" key={i}>
+                <div className="icon">{footerIcons[icon]}</div>
+                <div className="footer-contact-text">
+                  <p>{title}</p>
+                  <p>{details}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="copyright-area">
+          <div className="container">
+            <div className="copyright-text">
+              <p>Copyright &copy; 2024, All Right Reserved</p>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
