ce673eb24d93ff60333d53c3fde7c8247814d1fa
Itsarun01 <hey.arunkumar05@gmail.com>
2024-12-25 21:46:27 +0530

    re design..

--- src/Components/UI/Footer.jsx
+++ src/Components/UI/Footer.jsx
@@ -2,7 +2,6 @@ import FooterData from "../../api/FooterData.json";
 import {FaLocationDot} from "react-icons/fa6";
 import {BiSolidPhoneCall} from "react-icons/bi";
 import {MdMarkEmailUnread} from "react-icons/md";
-import {NavLink} from "react-router-dom";
 const Footer = () => {
   const footerIcons = {
     MdPlace: <FaLocationDot />,
