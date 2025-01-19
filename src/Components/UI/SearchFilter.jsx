96bcd142f0e8d27fb2d4367095d20d0ead084d91
Itsarun01 <hey.arunkumar05@gmail.com>
2025-01-19 22:00:31 +0530

    search bar and filter region login done ui pending..

---
+++ src/Components/UI/SearchFilter.jsx
@@ -0,0 +1,43 @@
+const SearchFilter = ({search, setSearch, filter, setFilter}) => {
+  const inputHandler = (e) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+  };
+
+  const selectHandler = (e) => {
+    e.preventDefault();
+    setFilter(e.target.value);
+  };
+
+  console.log(search, filter);
+
+  return (
+    <>
+      <section className="section-searchFilter container">
+        <input
+          type="text"
+          placeholder="Search Bar"
+          value={search}
+          onChange={inputHandler}
+        />
+
+        <div>
+          <select
+            className="select-section"
+            value={filter}
+            onChange={selectHandler}
+          >
+            <option value="all">All</option>
+            <option value="africa">Africa</option>
+            <option value="americas">Americas</option>
+            <option value="asia">Asia</option>
+            <option value="europe">Europe</option>
+            <option value="oceania">Oceania</option>
+          </select>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default SearchFilter;
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -2,24 +2,54 @@ import {useEffect, useTransition, useState} from "react";
 import {getCountries} from "../api/CountriesApi";
 import Loader from "../Components/UI/Loader";
 import CountryCard from "../Components/UI/CountryCard";
+import SearchFilter from "../Components/UI/SearchFilter";
 
 const Country = () => {
   const [isPending, startTransition] = useTransition();
-
   const [countries, setCountries] = useState([]);
 
+  const [search, setSearch] = useState();
+  const [filter, setFilter] = useState("all");
+
   useEffect(() => {
+    // useTransition hook to delay rendering until data is fetched
     startTransition(async () => {
       const res = await getCountries();
       setCountries(res.data);
     });
   }, []);
 
-  if (isPending) return <Loader />;
+  const searchCountry = (country) => {
+    if (search) {
+      return country.name.common.toLowerCase().includes(search.toLowerCase());
+    } else {
+      return country;
+    }
+  };
+
+  const selectRegion = (country) => {
+    if (filter === "all") {
+      return country;
+    } else {
+      return country.region.toLowerCase().includes(filter);
+    }
+  };
+
+  const filterCountries = countries.filter(
+    (country) => searchCountry(country) && selectRegion(country)
+  );
+
   return (
     <section className="container country-section">
+      <SearchFilter
+        search={search}
+        setSearch={setSearch}
+        filter={filter}
+        setFilter={setFilter}
+      />
+      {isPending && <Loader />}
       <ul className="grid grid-four-col">
-        {countries.map((countryData, index) => {
+        {filterCountries.map((countryData, index) => {
           return <CountryCard countryData={countryData} key={index} />;
         })}
       </ul>
